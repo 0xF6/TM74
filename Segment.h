205b46b962a4f7d18df571a21f986d3f5b1b5a57
Yuuki Wesp <ls-micro@ya.ru>
2020-01-25 23:28:59 +0300

    Create Segment.h

---
+++ Segment.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef __SEGMENT__
+#define __SEGMENT__
+#include "Arduino.h"
+
+typedef uint8_t u2_t;
+#define FALSE 0
+#define TRUE  1
+#define INVALID_SEGMENT 0b00110110
+
+struct Segment{
+	public:
+		Segment(uint8_t number) 
+            : Segment(number, FALSE) { };
+		Segment(uint8_t number, u2_t dot) 
+            : Segment(static_cast<char>(number), dot) { };
+		Segment(char symbol)
+            : Segment(symbol, FALSE) { };
+		Segment(char symbol, u2_t dot);
+
+		unsigned char getRaw() { return this->raw; }
+		u2_t isDot () { return this->dot; }
+	protected:
+        u2_t findInFonts(char symbol);
+		unsigned char cast2raw(char symbol);
+		char toLower_r(char symbol);
+
+		unsigned char raw;
+		u2_t    dot;
+};
+
+#endif
