8ccdbf2bf509c91234278003d780d50bff053938
Yuuki Wesp <ls-micro@ya.ru>
2020-01-25 23:28:40 +0300

    Create TM74.h

---
+++ TM74.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef TM74_h
+#define TM74_h
+#include "Segment.h"
+class TM74
+{
+  public:
+    TM74(int pinSCLK, int _pinRCLK, int pinDIO);
+    void write(unsigned char X);
+	void write(unsigned char X, unsigned char port);
+	void writeWithDot(unsigned char X, unsigned char port);
+
+	void writeSegments(const Segment segs[], uint8_t size);
+
+	void clear();
+  protected:
+    void reset();
+    void relock(const uint8_t flag = 0x1);
+
+	int _pinRCLK;
+	int _pinDIO;
+	int _pinSCLK;
+};
+
+#endif
